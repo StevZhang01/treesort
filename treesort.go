b4d61b5c1480d6da59d98b7c1aae2ad1f08efe8b
StevE Zhang <stevzhang01@gmail.com>
2019-01-10 12:26:17 +0800

    利用二叉树实现插入排序

---
+++ treesort.go
@@ -0,0 +1,41 @@
+// Package treesort 利用二叉树实现插入排序
+package treesort
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort 就地排序
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// addVvalues 依次追加root下的值到values
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+// 构建二叉树分支
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
